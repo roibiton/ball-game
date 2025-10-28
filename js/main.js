381ec6c40e1e345e65c8b5847c7b18d434cf91e6
roi biton <roibitonroi@gmail.com>
2025-10-28 18:59:21 +0200

    Add growth limit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 'use strict'
-
+var gTimeOut
 function onBallClick(elBall) {
+    if (gTimeOut) clearTimeout(gTimeOut)
     var currWidth = elBall.offsetWidth
     var currHeight = elBall.offsetHeight
     elBall.style.width = (currWidth + 50) + 'px'
     elBall.style.height = (currHeight + 50) + 'px'
-    elBall.innerText = currWidth + 50
+    gTimeOut = setTimeout(() => {
+        if (elBall.offsetWidth >= 400 || elBall.offsetHeight >= 400) {
+            elBall.style.width = 100 + 'px'
+            elBall.style.height = 100 + 'px'
+            elBall.innerText=100
+        }
+    }, 1000)
+    elBall.innerText = elBall.offsetWidth + 50
+
 }
