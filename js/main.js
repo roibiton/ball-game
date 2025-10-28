77fa6326b969da77ba108b7c29e3854a089e0b43
roi biton <roibitonroi@gmail.com>
2025-10-28 18:43:15 +0200

    Add onClick function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 'use strict'
 
-function onBallClick(elBall){
-    console.log('hi')
-    
-
+function onBallClick(elBall) {
+    var currWidth = elBall.offsetWidth
+    var currHeight = elBall.offsetHeight
+    elBall.style.width = (currWidth + 50) + 'px'
+    elBall.style.height = (currHeight + 50) + 'px'
+    elBall.innerText = currWidth + 50
 }
