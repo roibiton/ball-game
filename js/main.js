b5bd6d0201b12889f964bd274d9f50c59ad49ed6
roi biton <roibitonroi@gmail.com>
2025-10-29 00:28:39 +0200

    Add forth ball

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function onBallClick(elBall, maxSize = 500) {
     if (gTimeOut) clearTimeout(gTimeOut)
     var currWidth = elBall.offsetWidth
     var currHeight = elBall.offsetHeight
-    var randNum = getRandomInt(20, 60)
+    var randNum = getRandomInt(20, 61)
     var randColor = getRandomColor()
     elBall.style.width = (currWidth + randNum) + 'px'
     elBall.style.height = (currHeight + randNum) + 'px'
@@ -15,15 +15,38 @@ function onBallClick(elBall, maxSize = 500) {
             elBall.style.height = 100 + 'px'
             elBall.innerText = 100
         }
-    }, 1000)
+    }, 500)
     elBall.innerText = elBall.offsetWidth + randNum
 }
 
-function onBallClickChanger() {
+function onClickChanger() {
     var elBall = document.querySelector('.ball1')
-    onBallClick(elBall, 500)
+    var limitStr = elBall.getAttribute('onclick').split(',')[1]
+    var prevOnClickLimit = parseInt(limitStr.trim())
+    onBallClick(elBall, prevOnClickLimit)
     setTimeout(() => {
         elBall = document.querySelector('.ball2')
-        onBallClick(elBall, 300)
+        var limitStr = elBall.getAttribute('onclick').split(',')[1]
+        var prevOnClickLimit = parseInt(limitStr.trim())
+        onBallClick(elBall, prevOnClickLimit)
     }, 1000)
+}
+function onClickMaxSizeChanger() {
+    var randReducer = getRandomInt(20, 61)
+    var elBall1 = document.querySelector('.ball1')
+    var elBall2 = document.querySelector('.ball2')
+
+    var limitStr = elBall1.getAttribute('onclick').split(',')[1]
+    var prevOnClickLimit1 = parseInt(limitStr.trim())
+    var newMaxSize1 = prevOnClickLimit1 - randReducer
+    if (newMaxSize1 < 100) newMaxSize1 = 100
+    elBall1.setAttribute('onclick', `onBallClick(this, ${+newMaxSize1})`)
+    console.log(`Ball 1 max size changed to: ${newMaxSize1}`)
+
+    var limitStr2 = elBall2.getAttribute('onclick').split(',')[1]
+    var prevOnClickLimit2 = parseInt(limitStr2.trim())
+    var newMaxSize2 = prevOnClickLimit2 - randReducer
+    if (newMaxSize2 < 100) newMaxSize2 = 100
+    elBall2.setAttribute('onclick', `onBallClick(this, ${+newMaxSize2})`)
+    console.log(`Ball 2 max size changed to: ${newMaxSize2}`)
 }
